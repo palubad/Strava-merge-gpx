4730a30af727c6b569e19c6fd22be9e60b46dc81
Daniel Paluba <33784015+palubad@users.noreply.github.com>
2025-01-28 09:51:10 +0100

    Merge by time + add the app interface

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,35 @@ def merge_gpx_files(files):
         merged_gpx.tracks.append(new_track)
 
     return merged_gpx.to_xml()
+
+# Streamlit app
+st.title("GPX File Merger")
+st.write("Upload multiple GPX files to merge them into a single file.")
+
+# File uploader
+uploaded_files = st.file_uploader(
+    "Upload GPX files", 
+    type=["gpx"], 
+    accept_multiple_files=True
+)
+
+if uploaded_files:
+    # Button to trigger the merge process
+    if st.button("Merge Files"):
+        with st.spinner("Merging files..."):
+            # Merge GPX files
+            merged_gpx_data = merge_gpx_files(uploaded_files)
+
+            # Convert merged data to a downloadable file
+            merged_file = io.BytesIO()
+            merged_file.write(merged_gpx_data.encode('utf-8'))
+            merged_file.seek(0)
+
+            # Provide a download link
+            st.success("Files merged successfully!")
+            st.download_button(
+                label="Download Merged GPX File",
+                data=merged_file,
+                file_name="merged_output.gpx",
+                mime="application/gpx+xml"
+            )
